4845e4324ecfd73c59a0291bcd24a15e1af70b9a
JosnCat <evgeniy.prudnikovv@yandex.ru>
2016-10-16 18:18:13 +0300

    upd

---
+++ Algorithms/Labs/lab4/matrix_transpose/matrix_transpose.cpp
@@ -0,0 +1,48 @@
+// matrix_transpose.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	const int n = 2;
+	const int m = 4;
+	srand(static_cast<unsigned int>(time(NULL)));
+
+
+	vector<char> matrix(n*m);
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			matrix[i*m + j] = rand() % 256;
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << matrix[i*m + j];
+		}
+		cout << endl;
+	}
+
+
+	ofstream out("input.bin", ios::out | ios::binary);
+	
+	out.write((char *)&n, sizeof n);
+	out.write((char *)&m, sizeof m);
+	copy(matrix.begin(), matrix.end(), std::ostreambuf_iterator<char>(out));
+
+	out.close();
+	cout << endl<< "done." << endl;
+	getchar();
+	return 0;
+}
