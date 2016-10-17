8e4a8d16ee7bb9be0fbd349cb018d899b6b6dbc8
JonsCat <evgeniy.prudnikovv@yandex.ru>
2016-10-17 22:22:49 +0300

    upd

---
+++ PythonCourse/labs/lab7.py
@@ -0,0 +1,8 @@
+def lol (n):
+    res = []
+    [res.append([i,j,k]) for i in xrange(3,n) for j in xrange(i,n) for k in xrange(j,n) if (i**2 + j**2 == k**2 and n>0)]
+
+    return res
+
+print lol(200)
+
