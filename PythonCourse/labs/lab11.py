67049e26f9767141223da6291fce61898c650c48
JonsCat <Evgeniy.prudnikovv@yandex.ru>
2016-10-18 17:33:20 +0300

    upd lab11 py

--- PythonCourse/labs/lab11.py
+++ PythonCourse/labs/lab11.py
@@ -1,38 +1,48 @@
 class NVector:
     def __init__(self, obj):
+
         if isinstance(obj, list):
-            self.vector = obj
-            self.length = len(obj)
+            self.__vector = obj[:]
+            self.__length = len(obj)
         else:
-            self.vector = [obj]
-            self.length = 1
+            self.__vector = [obj]
+            self.__length = 1
 
     def __getitem__(self, item):
-        return self.vector[item]
+        return self.__vector[item]
 
     def __str__(self):
-        return str(self.vector)
+        return str(self.__vector)
 
     def __len__(self):
-        return len(self.vector)
+        return len(self.__vector)
 
     def __abs__(self):
-        return NVector([abs(i) for i in self.vector])
+        return NVector([abs(i) for i in self.__vector])
 
     def __add__(self, other):
-        return NVector([self.vector[i] + other[i] for i, e in enumerate(self.vector)])
+        return NVector([self.__vector[i] + other[i] for i, e in enumerate(self.__vector)])
 
     def __mul__(self, other):
-        return NVector([self.vector[i] * other for i, e in enumerate(self.vector)])
+        if isinstance(other, (int, float)):
+            return NVector([self.__vector[i] * other for i, e in enumerate(self.__vector)])
+        else:
+            return sum([self.__vector[i] * other[i] for i, e in enumerate(self.__vector)])
 
     def __sub__(self, other):
-        return NVector([self.vector[i] - other[i] for i, e in enumerate(self.vector)])
+        return NVector([self.__vector[i] - other[i] for i, e in enumerate(self.__vector)])
 
     def __eq__(self, other):
-        return all([self.vector[i] == other[i] for i, e in enumerate(self.vector)])
+        return all([self.__vector[i] == other[i] for i, e in enumerate(self.__vector)])
 
-a = NVector([1, -2, 3, 4, 5])
+    def __pow__(self, power, modulo=None):
+        return NVector([self.__vector[i] ** power for i, e in enumerate(self.__vector)])
+
+
+lol = [1, -2, 3, 4, 5]
+
+a = NVector(lol)
 b = NVector([1, -2, 3, 4, 7])
 
-print a == b
+print a
 print
