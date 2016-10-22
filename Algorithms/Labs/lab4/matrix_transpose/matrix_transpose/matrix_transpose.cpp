7f6211f031ff072899f64ac7ce13421cf7797c2a
JonsCat <evgeniy.prudnikovv@yandex.ru>
2016-10-22 18:30:09 +0300

    upd

--- Algorithms/Labs/lab4/matrix_transpose/matrix_transpose/matrix_transpose.cpp
+++ Algorithms/Labs/lab4/matrix_transpose/matrix_transpose/matrix_transpose.cpp
@@ -17,10 +17,12 @@ void Transpose( vector<unsigned char > & buf, int row, int col );
 int main()
 {
 
+    const auto startTime = std::clock();
+
 	int n;
 	int m;
 
-	//CreateTestFile();
+	CreateTestFile();
 
 	ifstream infile("input.bin", ios::in | ios::binary);
 	ofstream outfile("output.bin", ios::out | ios::binary);
@@ -28,59 +30,57 @@ int main()
 	infile.read(reinterpret_cast<char *>(&n), 4);
 	infile.read(reinterpret_cast<char *>(&m), 4);
 
-	cout << endl << n << endl;
-	cout << m << endl;
-
-
-	const int p = 256;
-	const int q = 256;
-
+	int p = 3;
+	int q = 3;
 
 	vector<unsigned char> buffer(p * q, 0);
 
-	for (int k = 0; k < n/p; ++k)
-	{
-		for (int u = 0; u < m / p; ++u)
-		{
-			for (int i = 0; i < p; ++i)
-			{
+    for (int k = 0; k < n/p; ++k)
+    {
+        for (int u = 0; u < m / p; ++u)
+        {
+            for (int i = 0; i < p; ++i)
+            {
 
-				infile.read((char *) &buffer[i * p], p);
-				infile.seekg(m - p, ios::cur);
-			}
-			infile.seekg(-m * p + p, ios::cur);
+                infile.read((char *) &buffer[i * p], p);
+                infile.seekg(m - p, ios::cur);
+            }
+            infile.seekg(-m * p + p, ios::cur);
 
-			Transpose(buffer, p, q);
+            Transpose(buffer, p, q);
 
-			for (int i = 0; i < p; ++i)
-			{
+            for (int i = 0; i < p; ++i)
+            {
 
-				outfile.write((char *) &buffer[i * p], p);
-				outfile.seekp(m - p, ios::cur);
-			}
-		}
-		infile.seekg(m * (p - 1), ios::cur);
-		outfile.seekp((-n * m) + p, ios::cur);
-	}
+                outfile.write((char *) &buffer[i * p], p);
+                outfile.seekp(n - p, ios::cur);
+            }
+        }
+        infile.seekg(m * (p - 1), ios::cur);
+        outfile.seekp((-n * m) + p, ios::cur);
+    }
 
 	infile.close();
 	outfile.close();
 
-	/*
+    cout<<endl;
+
 	ifstream infile1("output.bin", ios::in | ios::binary);
-	vector<unsigned char> buffer1(225, 0);
+	vector<unsigned char> buffer1(135, 0);
 	infile1.read((char *)&buffer1[0],buffer1.size());
-	for (int j = 0; j < n; ++j)
+
+
+	for (int i = 0; i < m; ++i)
 	{
-		for (int i = 0; i < m; ++i)
+		for (int j = 0; j < n; ++j)
 		{
-			cout<<(int)buffer1[j*m+i]<<" ";
+			cout<<(int)buffer1[i*n+j]<<" ";
 		}
 		cout<<endl;
 	}
-	*/
 
-	cout << endl << "done." << endl;
+    const auto endTime = std::clock();
+    //std::cerr << "done in  " << double(endTime - startTime) / CLOCKS_PER_SEC << '\n';
 	//getchar();
 	return 0;
 }
@@ -114,8 +114,8 @@ void printBuf(vector<unsigned  char> & buf)
 
 void CreateTestFile()
 {
-	const int n = 4096;
-	const int m = 4096;
+	const int n = 15;
+	const int m = 9;
 	//srand(static_cast<unsigned int>(time(NULL)));
 
 
@@ -127,7 +127,7 @@ void CreateTestFile()
 			matrix[i*m + j] = rand() % 256;
 		}
 	}
-	/*
+
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < m; ++j)
@@ -136,7 +136,7 @@ void CreateTestFile()
 		}
 		cout << endl;
 	}
-	*/
+
 	ofstream out("input.bin", ios::out | ios::binary);
 
 	out.write((char *)&n, sizeof(int));
