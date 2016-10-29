710fb0366a572c934758980de00388efbe91de6c
JosnCat <evgeniy.prudnikovv@yandex.ru>
2016-10-29 15:53:33 +0300

    upd

--- Algorithms/Labs/lab4/matrix_mult/matrix_mult/matrix_mult_clear.cpp
+++ Algorithms/Labs/lab4/matrix_mult/matrix_mult/matrix_mult_clear.cpp
@@ -1,28 +1,18 @@
-// matrix_mult.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <ctime>
-#include <iomanip>
+
 
 using namespace std;
 
-void printBuf(vector< int> & buf, int n, int m);
-void CreateTestFile();
 void Mult(vector<int> &a, vector<int> &  b, vector<int> &c, int size_i, int size_j, int size_k);
 
 
 int main()
 {
-	const auto startTime = std::clock();
-
 	int n;
 	int m;
-
-	CreateTestFile();
 
 	ifstream infileA("input.bin", ios::in | ios::binary);
 	ifstream infileB("input.bin", ios::in | ios::binary);
@@ -35,7 +25,7 @@ int main()
 	outfile.write(reinterpret_cast<char *>(&m), 4);
 
 
-	int block_size = 2;
+	int block_size = 128;
 
 	vector<int> bufferA;
 	vector<int> bufferB;
@@ -75,7 +65,7 @@ int main()
 				if (-n*(size_i - 1) !=0)
 					infileA.seekg(-n*(size_i - 1), ios::cur);
 
-				
+
 				bufferB.resize(size_k*size_j, 0);
 
 				for (int kb = 0; kb < size_k; kb++)
@@ -91,42 +81,33 @@ int main()
 
 					infileB.seekg(n - size_j, ios::cur);
 				}
-				//cout << "b";
-				//printBuf(bufferB, size_k, size_j);
-				//infileB.seekg(n - size_j, ios::cur);
-
 				Mult(bufferA, bufferB, bufferC, size_i, size_j, size_k);
-				//cout << (int)infileB.tellg() - 16 << endl;
 
 			}
 			bufferA.clear();
 			bufferB.clear();
 
 			// write
-			cout << (int)outfile.tellp() - 8 << endl;
+
 			for (int ib = 0; ib < size_i; ib++)
 			{
 				vector<char> line(size_j,0);
 				for (int jb = 0; jb < size_j; ++jb)
 				{
-					line[jb] = (char)(bufferC[ib * size_j + jb] /*% 256*/);
+					line[jb] = (char)(bufferC[ib * size_j + jb] % 256);
 				}
 
 				outfile.write(&line[0], size_j);
 				if (ib != size_i - 1) outfile.seekp(n - size_j, ios::cur);
 			}
-			cout << (int)outfile.tellp() - 8 << endl;
 			outfile.seekp(-n * (size_i - 1), ios::cur);
-			cout << (int)outfile.tellp() - 8 << endl;
 
 			bufferC.clear();
 			// seek a start
 			// b seek next column
-			//cout << "seek a to start b to next col" << endl;
+
 			infileA.seekg(-n, ios::cur);
 			infileB.seekg((-n * n) + size_j , ios::cur);
-
-
 		}
 
 		//seek A to next row
@@ -142,30 +123,6 @@ int main()
 	infileB.close();
 	infileA.close();
 	outfile.close();
-
-	cout << endl;
-	ifstream infile1("output.bin", ios::in | ios::binary);
-
-	int z;
-	int x;
-	infile1.read(reinterpret_cast<char *>(&z), 4);
-	infile1.read(reinterpret_cast<char *>(&x), 4);
-	vector<char> buffer1(z * x, 0);
-
-	infile1.read(&buffer1[0], buffer1.size());
-	for (int i = 0; i < z; ++i)
-	{
-		for (int j = 0; j < x; ++j)
-		{
-			cout << setiosflags(ios::fixed | ios::left) << setw(3) << (int)buffer1[i * x + j] << " ";
-		}
-		cout << endl;
-	}
-
-	const auto endTime = std::clock();
-	std::cout << endl << "done in  " << double(endTime - startTime) / CLOCKS_PER_SEC << '\n';
-	getchar();
-
 	return 0;
 }
 
@@ -181,78 +138,4 @@ void Mult(vector<int> &a, vector<int> &  b,vector<int> &c, int size_i,int size_j
 			}
 		}
 	}
-}
-
-void CreateTestFile()
-{
-	const int n = 4;
-	const int m = 4;
-	//srand(static_cast<unsigned int>(time(NULL)));
-
-
-	vector<unsigned char> matrixA(n * m);
-	vector<unsigned char> matrixB(n * m);
-	int c = 1;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			matrixA[i * m + j] = i;
-			c++;
-		}
-	}
-
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			matrixB[i * m + j] =j;
-			c++;
-		}
-	}
-
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			cout << setiosflags(ios::fixed | ios::left) << setw(3) << (int)matrixA[i * m + j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			cout << setiosflags(ios::fixed | ios::left) << setw(3) << (int)matrixB[i * m + j] << " ";
-		}
-		cout << endl;
-	}
-
-	ofstream out("input.bin", ios::out | ios::binary);
-
-	out.write((char *)&n, sizeof(int));
-	out.write((char *)&m, sizeof(int));
-	copy(matrixA.begin(), matrixA.end(), std::ostreambuf_iterator<char>(out));
-
-	out.write((char *)&n, sizeof(int));
-	out.write((char *)&m, sizeof(int));
-	copy(matrixB.begin(), matrixB.end(), std::ostreambuf_iterator<char>(out));
-
-	out.close();
-}
-
-void printBuf(vector< int> & buf,int n,int m)
-{
-	cout << endl;
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			cout << setiosflags(ios::fixed | ios::left) << setw(3) << (int)buf[i * m + j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
 }
