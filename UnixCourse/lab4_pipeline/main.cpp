ccb73cb1b67c3ff42b11736396148c6cd3de67cc
JonsCat <evgeniy.prudnikovv@yandex.ru>
2016-11-13 22:14:18 +0300

    upd

--- UnixCourse/lab4_pipeline/main.cpp
+++ UnixCourse/lab4_pipeline/main.cpp
@@ -7,81 +7,124 @@
 
 using namespace std;
 
-char* ARGS[2][5] = {
-        { "ls", "-l", "/etc/", NULL },
-        {"wc",          "-l", NULL }
-};
 
-int main(int argc , char* argv[]) {
+int main(int argc, char *argv[])
+{
 
-    std::vector<std::vector<char*>> a;
+    vector<vector<char *>> commands;
 
-    char* sep = (char *) "|";
+    char *sep = (char *) "|";
 
     int save = 1;
-    int j = 0;
-    while (save<argc) {
-        a.push_back(std::vector<char*> ());
-        for (int i = save; i < argc ; ++i)
+    int cmd_cnt = 0;
+    while (save < argc)
+    {
+        commands.push_back(vector<char *>());
+
+        for (int i = save; i < argc; ++i)
         {
-            if (!strcmp(argv[i],sep))
+            if (!strcmp(argv[i], sep))
             {
                 save++;
                 break;
             }
-            char* lol = argv[i];
-            a[j].push_back(lol);
+
+            commands[cmd_cnt].push_back(argv[i]);
 
             save++;
         }
-        j++;
+        cmd_cnt++;
     }
 
 
+    int pipefds[cmd_cnt - 1][2];
 
-    int pipefds[j][2];
-
-    for (int k = 0; k < j; ++k)
+    for (int k = 0; k < cmd_cnt - 1; ++k)
     {
-        if (pipe((int*)pipefds[k]) == -1) {
+        if (pipe((int *) pipefds[k]) == -1)
+        {
             perror("pipe");
             return 1;
         }
     }
 
+    pid_t children[cmd_cnt];
 
-    pid_t children[j];
+    for (int i = 0; i < cmd_cnt; ++i)
+    {
+        int curr_pipe = i == 0 ? 0 : i - 1;
+        int next_pipe = curr_pipe + 1;
 
-    for (int i = 0; i < 2; ++i) {
         pid_t pid = fork();
 
-        if (pid == 0) {
-            char** args = ARGS[i];
-            if (i == 0) {
-                dup2(pipefds[1], 1);
-                //close(pipefds[1]);
-                close(pipefds[0]);
-            } else if (i == 1) {
-                dup2(pipefds[0], 0);
-                //close(pipefds[0]);
-                close(pipefds[1]);
+        if (pid == 0)
+        {
+
+            char **args = commands[i].data();
+
+
+            if (i == 0)
+            {
+
+                dup2(pipefds[curr_pipe][1], 1);
+                close(pipefds[curr_pipe][0]);
+
+
+            } else if (i == cmd_cnt - 1)
+            {
+                dup2(pipefds[curr_pipe][0], 0);
+
+                for (int j = 0; j < next_pipe ; ++j)
+                {
+                    close(pipefds[j][1]);
+                }
             }
+            else
+            {
+                dup2(pipefds[curr_pipe][0], 0);
+                dup2(pipefds[next_pipe][1], 1);
+
+                for (int j = 0; j <= next_pipe ; ++j)
+                {
+                    if (j == next_pipe)
+                    {
+                        close(pipefds[j][0]);
+                    }
+                    else
+                    {
+                        close(pipefds[j][1]);
+                    }
+                }
+            }
+
+
             execvp(args[0], args);
-        } else {
+
+
+        } else
+        {
             children[i] = pid;
         }
     }
 
-    for (int i = 0; i < 2; ++i) {
-        close(pipefds[i]);
+    for (int l = 0; l < cmd_cnt - 1; ++l)
+    {
+        for (int i = 0; i < 2; ++i)
+        {
+            close(pipefds[l][i]);
+        }
     }
+
 
     int exitCode = 0;
     int status;
-    for (int i = 0; i < 2; ++i) {
+    for (int i = 0; i < cmd_cnt; ++i)
+    {
         waitpid(children[i], &status, 0);
-        if (WIFEXITED(status)) {
-            if (WEXITSTATUS(status) != 0) {
+        if (WIFEXITED(status))
+        {
+            if (WEXITSTATUS(status) != 0)
+            {
                 exitCode = 1;
             }
         }
