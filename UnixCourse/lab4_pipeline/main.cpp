6f554a15723934b65ad0b3b7c7fb0f9cf5493a48
JonsCat <evgeniy.prudnikovv@yandex.ru>
2016-11-13 23:38:52 +0300

    upd

--- UnixCourse/lab4_pipeline/main.cpp
+++ UnixCourse/lab4_pipeline/main.cpp
@@ -10,28 +10,53 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-
+    int exitCode = 0;
     vector<vector<char *>> commands;
 
     char *sep = (char *) "|";
 
-    int save = 1;
+    int param_p = 1;
+    int sep_p = 0;
     int cmd_cnt = 0;
-    while (save < argc)
+    while (param_p < argc)
     {
         commands.push_back(vector<char *>());
 
-        for (int i = save; i < argc; ++i)
+        for (int i = param_p; i < argc; ++i)
         {
             if (!strcmp(argv[i], sep))
             {
-                save++;
+
+                if (param_p == 1)
+                {
+                    cerr << "Wrong arguments before | " << endl;
+                    exitCode = 2;
+                    return exitCode;
+                }
+
+                if (param_p == sep_p + 1)
+                {
+                    cerr << "Wrong arguments between | " << endl;
+                    exitCode = 2;
+                    return exitCode;
+                }
+
+                sep_p = param_p;
+                param_p++;
+
+                if (param_p == argc)
+                {
+                    cerr << "Wrong arguments after | " << endl;
+                    exitCode = 2;
+                    return exitCode;
+                }
+
                 break;
             }
 
             commands[cmd_cnt].push_back(argv[i]);
 
-            save++;
+            param_p++;
         }
         cmd_cnt++;
     }
@@ -74,32 +99,28 @@ int main(int argc, char *argv[])
             {
                 dup2(pipefds[curr_pipe][0], 0);
 
-                for (int j = 0; j < next_pipe ; ++j)
+                for (int j = 0; j < next_pipe; ++j)
                 {
                     close(pipefds[j][1]);
                 }
-            }
-            else
+            } else
             {
                 dup2(pipefds[curr_pipe][0], 0);
                 dup2(pipefds[next_pipe][1], 1);
 
-                for (int j = 0; j <= next_pipe ; ++j)
+                for (int j = 0; j <= next_pipe; ++j)
                 {
                     if (j == next_pipe)
                     {
                         close(pipefds[j][0]);
-                    }
-                    else
+                    } else
                     {
                         close(pipefds[j][1]);
                     }
                 }
             }
 
-
             execvp(args[0], args);
-
 
         } else
         {
@@ -116,7 +137,6 @@ int main(int argc, char *argv[])
     }
 
 
-    int exitCode = 0;
     int status;
     for (int i = 0; i < cmd_cnt; ++i)
     {
