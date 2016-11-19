d18ecc1b4a1047ec964a7068319b81c68a347984
JonsCat <evgeniy.prudnikovv@yandex.ru>
2016-11-19 21:03:30 +0300

    python lab 16

--- PythonCourse/labs/lab16.py
+++ PythonCourse/labs/lab16.py
@@ -1,10 +1,14 @@
 def my_xrange(start, stop=None, step=None):
-    x = 0
+    x = 0 if stop is None else start
+    end = start if stop is None else stop
     x_step = 1 if step is None else step
-    while x <= start:
+    while x < end:
         yield x
         x += x_step
 
 
-print list(my_xrange(10, step=3))
-print list(my_xrange(10, step=3))
+print list(my_xrange(10, 15, 1))
+print list(xrange(10, 15, 1))
+
+print type(my_xrange)
+print type(xrange)
