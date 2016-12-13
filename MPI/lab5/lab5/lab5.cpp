c5c12adea4827ada97c1f5136e4abe2a2103490c
JonsCat <Evgeniy.prudnikovv@yandex.ru>
2016-12-13 16:18:23 +0300

    upd l5

--- MPI/lab5/lab5/lab5.cpp
+++ MPI/lab5/lab5/lab5.cpp
@@ -1,6 +1,9 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+
+using namespace std;
 
 const int N1 = 10;
 const int N2 = 10;
@@ -14,37 +17,47 @@ int main(int argc, char** argv)
 	MPI_Status stat;
 	MPI_Request req;
 
-	printf("lols");
-
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_of_procs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
 
-
-
 	int rows_per_p = N1 / num_of_procs; // 2
 	int num_of_blk_per_p = N2 / block_size; //5
 
-	int **p_tape = (int **)malloc(rows_per_p * sizeof(int));
-
+	int **p_tape = (int **)calloc(rows_per_p, sizeof(int));
+	if (!p_tape)
+	{
+		printf("bad allocation");
+		return 2;
+	}
 	for (int i = 0; i < rows_per_p; i++)
 	{
-		p_tape[i] = (int *)malloc(N2 * sizeof(int));
-	}
+		p_tape[i] = (int *)calloc(N2, sizeof(int));
 
+		if (!p_tape[i])
+		{
+			printf("bad allocation");
+			return 2;
+		}
+	}
 
 	if (proc_rank == 0)
 	{
+		int buff_send[block_size];
 		for (int k = 0; k < num_of_blk_per_p; k++)
 		{
 			for (int i = 1; i < rows_per_p; i++)
 			{
-				for (int j = 0; j < block_size; j++)
+				int c = 0;
+				for (int j = k*block_size; j < (k + 1)*block_size; j++)
 				{
 					p_tape[i][j] = p_tape[i - 1][j] + 1;
+					if (i == rows_per_p - 1) buff_send[c] = p_tape[i][j];
+					c++;
 				}
+
 			}
-			MPI_Send(p_tape[rows_per_p - 1] + k*block_size * sizeof(int), block_size, MPI_INT, proc_rank + 1, proc_rank + 1, MPI_COMM_WORLD /*, &req*/);
+			MPI_Send(buff_send, block_size, MPI_INT, proc_rank + 1, k, MPI_COMM_WORLD /*, &req*/);
 		}
 
 		for (int v = 0; v < rows_per_p; v++)
@@ -55,26 +68,29 @@ int main(int argc, char** argv)
 			}
 			printf("\n");
 		}
+		printf("\n");
 	}
 	else if (proc_rank == num_of_procs - 1)
 	{
 		for (int k = 0; k < num_of_blk_per_p; k++)
 		{
-			int buff[block_size];
-			MPI_Recv(buff, block_size, MPI_INT, proc_rank - 1, proc_rank - 1, MPI_COMM_WORLD, &stat);
-
+			int buff_res[block_size];
+			MPI_Recv(buff_res, block_size, MPI_INT, proc_rank - 1, k, MPI_COMM_WORLD, &stat);
+			
 			for (int i = 0; i < rows_per_p; i++)
 			{
-				for (int j = 0; j < block_size; j++)
+				int c = 0;
+				for (int j = k*block_size; j < (k + 1)*block_size; j++)
 				{
 					if (i == 0)
 					{
-						p_tape[i][j] = buff[j] + 1;
+						p_tape[i][j] = buff_res[c] + 1;
 					}
 					else
 					{
 						p_tape[i][j] = p_tape[i - 1][j] + 1;
 					}
+					c++;
 				}
 			}
 		}
@@ -87,31 +103,36 @@ int main(int argc, char** argv)
 			}
 			printf("\n");
 		}
+		printf("\n");
 
 	}
 	else
 	{
 		for (int k = 0; k < num_of_blk_per_p; k++)
 		{
-			int buff[block_size];
-			MPI_Recv(buff, block_size, MPI_INT, proc_rank - 1, proc_rank - 1, MPI_COMM_WORLD, &stat);
-
+			int buff_res[block_size];
+			int buff_send[block_size];
+			MPI_Recv(buff_res, block_size, MPI_INT, proc_rank - 1, k, MPI_COMM_WORLD, &stat);
+			
 			for (int i = 0; i < rows_per_p; i++)
 			{
-				for (int j = 0; j < block_size; j++)
+				int c = 0;
+				for (int j = k*block_size; j < (k + 1)*block_size; j++)
 				{
 					if (i == 0)
 					{
-						p_tape[i][j] = buff[j] + 1;
+						p_tape[i][j] = buff_res[c] + 1;
 					}
 					else
 					{
 						p_tape[i][j] = p_tape[i - 1][j] + 1;
+						if (i == rows_per_p - 1) buff_send[c] = p_tape[i][j];
 					}
+					c++;
 				}
 			}
 
-			MPI_Send(p_tape[rows_per_p - 1] + k*block_size * sizeof(int), block_size, MPI_INT, proc_rank + 1, proc_rank + 1, MPI_COMM_WORLD/*, &req*/);
+			MPI_Send(buff_send, block_size, MPI_INT, proc_rank + 1, k, MPI_COMM_WORLD/*, &req*/);
 		}
 
 		for (int v = 0; v < rows_per_p; v++)
@@ -122,11 +143,8 @@ int main(int argc, char** argv)
 			}
 			printf("\n");
 		}
+		printf("\n");
 	}
-
-	free(p_tape);
-
 	MPI_Finalize();
-	//getchar();
 	return 0;
 }
