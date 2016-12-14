28eacf9c66283b7a95fcef8277f171b24958a263
JonsCat <Evgeniy.prudnikovv@yandex.ru>
2016-12-14 13:39:08 +0300

    upd

--- PythonCourse/labs/lab16.py
+++ PythonCourse/labs/lab16.py
@@ -1,29 +1,23 @@
 def my_xrange(start, stop=None, step=None):
+    assert isinstance(start, int), 'type error! an integer is required'
+    assert isinstance(stop, int), 'type error! an integer is required'
+    assert isinstance(step, int), 'type error! an integer is required'
+
     x = 0 if stop is None else start
     end = start if stop is None else stop
     x_step = 1 if step is None else step
 
-    # assert isinstance(start, int), 'type error!'
-    # assert isinstance(stop, int), 'type error!'
-    # assert isinstance(step, int), 'type error!'
-
-
-    while x < end:
-        yield x
-        x += x_step
+    if x < end:
+        if x_step <= 0: return
+        while x < end:
+            yield x
+            x += x_step
+    else:
+        if x_step >= 0: return
+        while x > end:
+            yield x
+            x += x_step
 
 
 print list(my_xrange(10, 0, -1))
 print list(xrange(10, 0, -1))
-
-print list(my_xrange(10))
-print list(xrange(10))
-
-print list(my_xrange(10, 0, -1))
-print list(xrange(10, 0, -1))
-
-print list(my_xrange(10, 0, -2.5))
-print list(xrange(10, 0, -2.5))
-
-print type(my_xrange)
-print type(xrange)
