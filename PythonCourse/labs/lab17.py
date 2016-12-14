0bb98c676cbce6bad28ccfb4d283acc59fa71b1c
JonsCat <Evgeniy.prudnikovv@yandex.ru>
2016-12-14 15:25:23 +0300

    upd

--- PythonCourse/labs/lab17.py
+++ PythonCourse/labs/lab17.py
@@ -1,39 +1,33 @@
 def compress(lst):
     el_cnt = 1
-    res = []
+
     for i in xrange(len(lst) - 1):
         if lst[i + 1] == lst[i]:
             el_cnt += 1
         else:
-            res.append(el_cnt)
-            res.append(lst[i])
+            yield el_cnt
+            yield lst[i]
             el_cnt = 1
-
-    res.append(el_cnt)
-    res.append(lst[len(lst) - 1])
-
-    return res
+    yield el_cnt
+    yield lst[len(lst) - 1]
 
 
 def decompress(lst):
     el_cnt = 1
-    res = []
     for i in xrange(len(lst)):
         if i % 2 == 0:
             el_cnt = lst[i]
             assert isinstance(el_cnt, int), 'Wrong sequence!'
         else:
             for j in xrange(el_cnt):
-                res.append(lst[i])
-
-    return res
+                yield lst[i]
 
 
-lol = ["hi", 'q', 'q', 'q', [1, 2], 3, 3, 6, 3, 3, 23, 'lol', 'lol', 'lol']
+lol = ["hi", 'q', 'q', 'q', [1, 2], 3, 3, 3, 3, 3, 23, 'lol', 'lol', 'lol']
 
-lol2 = compress(lol)
+lol2 = list(compress(lol))
 
-lol3 = decompress(lol2)
+lol3 = list(decompress(lol2))
 
 print lol
 print lol2
