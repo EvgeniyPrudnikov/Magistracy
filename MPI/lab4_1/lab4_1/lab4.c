2ca20ec248d026a0bdc9d5b69a5d23abe4d0ba3e
jonscat <evgeniy.prudnikovv@yandex.ru>
2017-01-03 23:57:45 +0300

    upd

--- MPI/lab4_1/lab4_1/lab4.c
+++ MPI/lab4_1/lab4_1/lab4.c
@@ -12,19 +12,20 @@ int main(int argc, char** argv)
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-	MPI_Status stat;
+    MPI_Status stat;
 
 	if (world_rank == 0)
 	{
+
 		char **buff = (char**)malloc((world_size - 1) * sizeof(char*));
 
-		for (int i = 1; i < world_size; i++)
+		for (int i = 0; i < world_size - 1; i++)
 		{
 			MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
 			int n;
 			MPI_Get_count(&stat, MPI_CHAR, &n);
-			buff[i - 1] = (char *)malloc(n * sizeof(char));
-			MPI_Recv(buff[i - 1], n, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+			buff[stat.MPI_SOURCE -1] = (char *)malloc(n * sizeof(char));
+			MPI_Recv(buff[stat.MPI_SOURCE - 1], n, MPI_CHAR, stat.MPI_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
 		}
 
 		for (int i = 0; i < world_size - 1; i++)
@@ -35,7 +36,7 @@ int main(int argc, char** argv)
 	else
 	{
 		char buf[25];
-		int len = sprintf(buf, "Hello from %d\n", world_rank);
+		int len = sprintf(buf, "Hello from %i\n", world_rank);
 		MPI_Send(buf, len + 1, MPI_CHAR, 0, world_rank, MPI_COMM_WORLD);
 	}
 
