a14ba6acc86b587fb25f4edc2f5532f8f40bbe63
jonscat <evgeniy.prudnikovv@yandex.ru>
2017-06-07 00:30:58 +0300

    upd

--- Diploma/python/an.py
+++ Diploma/python/an.py
@@ -214,6 +214,103 @@ counts_by_col_l4 = counts_by_col4.collect()
 
 plt_hist(counts_by_col_l4[0].asDict())
 
-#TODO: add corr matrix code
-#TODO: add analysis code
 
+
+pdf_date = df_date_smpl.toPandas()
+
+cols_date = [(c.split('_')) for c in pdf_date.columns]
+cols_date_df = pd.DataFrame(cols_date[1:], columns=['line','station','feature'])
+cols_date_df['full_name'] = cols_date_df.line + '_' + cols_date_df.station + '_' + cols_date_df.feature
+cols_date_df = cols_date_df.drop('line',1)
+for i in list(cols_date_df.station.unique()):
+    station_features = list(cols_date_df.loc[cols_date_df['station'] == i].full_name.values)
+    pdf_date[station_features].corr(method='pearson')
+
+#    L0_S0_D1    L0_S0_D3    L0_S0_D5    L0_S0_D7    L0_S0_D9    L0_S0_D11   L0_S0_D13   L0_S0_D15   L0_S0_D17   L0_S0_D19   L0_S0_D21   L0_S0_D23
+#L0_S0_D1    1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S0_D3    1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S0_D5    1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S0_D7    1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S0_D9    1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S0_D11   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S0_D13   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S0_D15   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S0_D17   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S0_D19   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S0_D21   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S0_D23   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#Out[79]:
+#L0_S1_D26   L0_S1_D30
+#L0_S1_D26   1.0 1.0
+#L0_S1_D30   1.0 1.0
+#Out[79]:
+#L0_S2_D34   L0_S2_D38   L0_S2_D42   L0_S2_D46   L0_S2_D50   L0_S2_D54   L0_S2_D58   L0_S2_D62   L0_S2_D66
+#L0_S2_D34   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S2_D38   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S2_D42   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S2_D46   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S2_D50   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S2_D54   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S2_D58   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S2_D62   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+#L0_S2_D66   1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+# ....
+
+# date df
+pdf_num = df_mun_smpl.toPandas()
+
+cols_date = [(c.split('_')) for c in pdf_num.columns]
+cols_date_df = pd.DataFrame(cols_date[1:], columns=['line','station','feature'])
+cols_date_df['full_name'] = cols_date_df.line + '_' + cols_date_df.station + '_' + cols_date_df.feature
+cols_date_df = cols_date_df.drop('line',1)
+for i in list(cols_date_df.station.unique()):
+    station_features = list(cols_date_df.loc[cols_date_df['station'] == i].full_name.values)
+    pdf_num[station_features].corr(method='pearson')
+
+# num df 
+useful_values = []
+cols_date = [(c.split('_')) for c in pdf_num.columns]
+cols_date_df = pd.DataFrame(cols_date[1:], columns=['line','station','feature'])
+cols_date_df['full_name'] = cols_date_df.line + '_' + cols_date_df.station + '_' + cols_date_df.feature
+cols_date_df = cols_date_df.drop('line',1)
+for i in list(cols_date_df.station.unique()):
+    station_features = list(cols_date_df.loc[cols_date_df['station'] == i].full_name.values)
+    corr_df = pd.DataFrame(pdf_num[station_features].corr(method='pearson'))
+    corr_df
+    try:
+        min_value = abs(corr_df.loc[:, station_features]).min(axis=1).idxmax(axis=0)
+    except:
+        min_value = None
+    if min_value:
+        useful_values.append(min_value)
+useful_values
+
+
+#    L0_S0_F0    L0_S0_F2    L0_S0_F4    L0_S0_F6    L0_S0_F8    L0_S0_F10   L0_S0_F12   L0_S0_F14   L0_S0_F16   L0_S0_F18   L0_S0_F20   L0_S0_F22
+#L0_S0_F0    1.000000    0.889585    -0.024860   -0.023693   -0.021955   -0.016040   -0.778816   -0.699263   0.151366    0.206989    0.153013    0.153011
+#L0_S0_F2    0.889585    1.000000    -0.023703   -0.027373   -0.042980   0.012173    -0.689760   -0.778402   0.223214    0.176426    0.199401    0.199399
+#L0_S0_F4    -0.024860   -0.023703   1.000000    0.992738    -0.028634   0.049657    0.551105    0.552818    0.056977    0.046345    -0.013667   -0.013668
+#L0_S0_F6    -0.023693   -0.027373   0.992738    1.000000    -0.029508   0.049524    0.554599    0.551547    0.057959    0.044261    -0.012808   -0.012810
+#L0_S0_F8    -0.021955   -0.042980   -0.028634   -0.029508   1.000000    0.516884    -0.038238   -0.021610   0.042460    0.080503    -0.050112   -0.050098
+#L0_S0_F10   -0.016040   0.012173    0.049657    0.049524    0.516884    1.000000    0.038773    0.016819    0.220418    0.270935    -0.000579   -0.000578
+#L0_S0_F12   -0.778816   -0.689760   0.551105    0.554599    -0.038238   0.038773    1.000000    0.914613    -0.098344   -0.158758   -0.109484   -0.109484
+#L0_S0_F14   -0.699263   -0.778402   0.552818    0.551547    -0.021610   0.016819    0.914613    1.000000    -0.161912   -0.138369   -0.140083   -0.140083
+#L0_S0_F16   0.151366    0.223214    0.056977    0.057959    0.042460    0.220418    -0.098344   -0.161912   1.000000    0.607189    0.018838    0.018839
+#L0_S0_F18   0.206989    0.176426    0.046345    0.044261    0.080503    0.270935    -0.158758   -0.138369   0.607189    1.000000    0.023059    0.023059
+#L0_S0_F20   0.153013    0.199401    -0.013667   -0.012808   -0.050112   -0.000579   -0.109484   -0.140083   0.018838    0.023059    1.000000    1.000000
+#L0_S0_F22   0.153011    0.199399    -0.013668   -0.012810   -0.050098   -0.000578   -0.109484   -0.140083   0.018839    0.023059    1.000000    1.000000
+#Out[106]:
+#L0_S1_F24   L0_S1_F28
+##L0_S1_F24   1.00000 -0.26028
+#L0_S1_F28   -0.26028    1.00000
+#Out[106]:
+#L0_S2_F32   L0_S2_F36   L0_S2_F40   L0_S2_F44   L0_S2_F48   L0_S2_F52   L0_S2_F56   L0_S2_F60   L0_S2_F64
+#L0_S2_F32   1.000000    -0.336864   0.755147    -0.146511   0.294490    NaN 0.694501    0.032475    -0.114641
+#L0_S2_F36   -0.336864   1.000000    0.022725    0.017860    -0.005609   NaN 0.039810    -0.016953   0.017920
+#L0_S2_F40   0.755147    0.022725    1.000000    -0.600806   0.168338    NaN 0.595915    0.019304    -0.276755
+#L0_S2_F44   -0.146511   0.017860    -0.600806   1.000000    0.065475    NaN -0.102562   -0.023918   0.187910
+#L0_S2_F48   0.294490    -0.005609   0.168338    0.065475    1.000000    NaN 0.211209    0.058574    0.005544
+#L0_S2_F52   NaN NaN NaN NaN NaN NaN NaN NaN NaN
+#L0_S2_F56   0.694501    0.039810    0.595915    -0.102562   0.211209    NaN 1.000000    -0.584736   -0.020423
+#L0_S2_F60   0.032475    -0.016953   0.019304    -0.023918   0.058574    NaN -0.584736   1.000000    -0.002484
+#L0_S2_F64   -0.114641   0.017920    -0.276755   0.187910    0.005544    NaN -0.020423   -0.002484   1.000000
--- Diploma/python/main3.py
+++ Diploma/python/main3.py
@@ -1,47 +1,43 @@
-import gc
-import numpy as np
-import pandas as pd
-import xgboost as xgb
-from sklearn.metrics import matthews_corrcoef
-from sklearn.metrics import precision_recall_curve
-from sklearn.metrics import average_precision_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics.scorer import make_scorer
+from sklearn.metrics import matthews_corrcoef
 from operator import itemgetter
 import matplotlib.pyplot as plt
 from numba import jit
+import xgboost as xgb
+import pandas as pd
+import numpy as np
 import datetime
+import gc
 
 DIRECTORY = '../../../diploma_data/'
-# TRAIN_FILES = ['tr_cat.csv', 'tr_date.csv', 'tr_num.csv']
 TRAIN_FILES = ['train_categorical.csv', 'train_date.csv', 'train_numeric.csv']
 
 
 def get_date_features():
     train_file = TRAIN_FILES[1]
-
     for i, chunk in enumerate(pd.read_csv(DIRECTORY + train_file, chunksize=1, low_memory=False)):
         features = list(chunk.columns)
         break
-
     seen = np.zeros(52)
     rv = []
     for f in features:
         if f == 'Id' or 'S24' in f or 'S25' in f:
             rv.append(f)
             continue
-
         station = int(f.split('_')[1][1:])
-
         if seen[station]:
             continue
-
         seen[station] = 1
         rv.append(f)
-
     return rv
 
 
-processed_date_features = get_date_features()
+useful_date_features = get_date_features()
 
 
 def get_min_max_date():
@@ -50,14 +46,12 @@ def get_min_max_date():
     features = None
     subset = None
 
-    for i, chunk in enumerate(pd.read_csv(DIRECTORY + train_file, usecols=processed_date_features, chunksize=50000,
+    for i, chunk in enumerate(pd.read_csv(DIRECTORY + train_file, usecols=useful_date_features, chunksize=50000,
                                           low_memory=False)):
         print i
-
         if features is None:
             features = list(chunk.columns)
             features.remove('Id')
-
         df_date_chunk = chunk[['Id']].copy()
         df_date_chunk['mindate'] = chunk[features].min(axis=1).values
         df_date_chunk['maxdate'] = chunk[features].max(axis=1).values
@@ -66,7 +60,6 @@ def get_min_max_date():
             subset = df_date_chunk.copy()
         else:
             subset = pd.concat([subset, df_date_chunk])
-
         del chunk
         gc.collect()
 
@@ -90,9 +83,7 @@ def get_min_max_date_df():
 
     return df_date
 
-
 prepared_df_date = get_min_max_date_df()
-
 
 @jit
 def mcc(tp, tn, fp, fn):
@@ -142,12 +133,17 @@ def eval_mcc(y_true, y_prob, show=False):
 
 
 def mcc_eval(y_prob, dtrain):
-    y_true = dtrain.get_label()
-    best_mcc = eval_mcc(y_true, y_prob)
-    return 'MCC', best_mcc
+    if isinstance(dtrain, np.ndarray):
+        y_true = dtrain
+        best_mcc = eval_mcc(y_true, y_prob)
+        return best_mcc
+    else:
+        y_true = dtrain.get_label()
+        best_mcc = eval_mcc(y_true, y_prob)
+        return 'MCC', best_mcc
 
 
-def plot_precision_recall(lines):
+def plot_precision_recall(lines, file_name):
     plt.clf()
 
     for precision, recall, avg_pres_score, label in lines:
@@ -158,7 +154,8 @@ def plot_precision_recall(lines):
     plt.xlim([0.0, 1.0])
     plt.title('Precision-Recall')
     plt.legend(loc="upper right")
-    plt.show()
+    #plt.show()
+    plt.savefig(file_name, format='png')
 
 
 def create_feature_map(features):
@@ -195,31 +192,36 @@ def leave_one_out(data1, data2, column_name, use_LOO=False):
 
 
 def data_transformation():
+    # as most useful features with lowest correlation between themselves and max correlation with target variable
     cols = [['Id',
-             'L1_S24_F1559', 'L3_S32_F3851',
-             'L1_S24_F1827', 'L1_S24_F1582',
-             'L3_S32_F3854', 'L1_S24_F1510',
-             'L1_S24_F1525'],
+             'L1_S24_F1559', 'L3_S32_F3851','L1_S24_F1827', 'L1_S24_F1582',
+             'L3_S32_F3854', 'L1_S24_F1510','L1_S24_F1525'],
             ['Id',
              'L3_S30_D3496', 'L3_S30_D3506',
              'L3_S30_D3501', 'L3_S30_D3516',
-             'L3_S30_D3511'],
+             'L3_S30_D3511'
+             ],
             ['Id',
-             'L1_S24_F1846', 'L3_S32_F3850',
-             'L1_S24_F1695', 'L1_S24_F1632',
-             'L3_S33_F3855', 'L1_S24_F1604',
-             'L3_S29_F3407', 'L3_S33_F3865',
-             'L3_S38_F3952', 'L1_S24_F1723',
+             'L0_S0_F12', 'L0_S1_F24', 'L0_S2_F32', 'L0_S3_F92',
+             'L0_S4_F104', 'L0_S5_F114', 'L0_S6_F118', 'L0_S7_F136',
+             'L0_S8_F149', 'L0_S9_F180', 'L0_S10_F264','L0_S11_F306',
+             'L0_S12_F346', 'L0_S13_F354', 'L0_S14_F366', 'L0_S15_F406',
+             'L0_S16_F421', 'L0_S17_F431', 'L0_S18_F439', 'L0_S19_F453',
+             'L0_S20_F466', 'L0_S21_F482', 'L0_S22_F586', 'L0_S23_F635',
+             'L1_S24_F1421', 'L1_S25_F2677', 'L2_S26_F3106', 'L2_S27_F3214',
+             'L2_S28_F3292', 'L3_S29_F3430', 'L3_S30_F3624', 'L3_S31_F3842',
+             'L3_S32_F3850', 'L3_S33_F3857', 'L3_S34_F3880', 'L3_S35_F3889',
+             'L3_S36_F3918', 'L3_S37_F3950', 'L3_S38_F3952', 'L3_S39_F3972',
+             'L3_S40_F3982', 'L3_S41_F4026', 'L3_S43_F4090', 'L3_S44_F4112',
+             'L3_S45_F4124', 'L3_S47_F4148', 'L3_S48_F4193', 'L3_S49_F4236',
+             'L3_S50_F4243', 'L3_S51_F4262',
              'Response']]
     train_data = None
 
     for k, f in enumerate(TRAIN_FILES):
         print f
         subset = None
-        for i, chunk in enumerate(pd.read_csv(DIRECTORY + f,
-                                              usecols=cols[k],
-                                              chunksize=50000,
-                                              low_memory=False)):
+        for i, chunk in enumerate(pd.read_csv(DIRECTORY + f, usecols=cols[k], chunksize=50000, low_memory=False)):
             print i
             if subset is None:
                 subset = chunk.copy()
@@ -251,13 +253,18 @@ def data_transformation():
     return blind_train_data
 
 
-def train_model():
+def get_train_test_data():
     train_data = data_transformation()
     print 'train size:', train_data.shape
     features = list(train_data.columns)
     features.remove('Response')
     features.remove('Id')
-    print features
+    X_train, X_test, y_train, y_test = train_test_split(train_data[features], train_data.Response, test_size=0.33,
+                                                        random_state=42)
+    return features, X_train, X_test, y_train, y_test
+
+
+def train_xgb_model(features, X_train, X_test, y_train, y_test):
     num_rounds = 50
     params = {
         'objective': "binary:logistic"
@@ -268,15 +275,13 @@ def train_model():
         , 'base_score': 0.005
         , 'silent': True
     }
-    print 'Fitting'
+    print 'Fitting XGBoost'
     train_predictions = None
-
-    X_train, X_test, y_train, y_test = train_test_split(train_data[features], train_data.Response, test_size=0.33, random_state=42)
 
     X_train = xgb.DMatrix(X_train[features], y_train, silent=True)
     X_test = xgb.DMatrix(X_test[features], y_test, silent=True)
 
-    folds = 1
+    folds = 5
     for i in range(folds):
         print 'Fold:', i
         params['seed'] = i
@@ -292,7 +297,7 @@ def train_model():
 
         predictions = clf.predict(X_test, ntree_limit=limit)
 
-        best_proba, best_mcc, y_pred = eval_mcc(y_test, (predictions / folds > 0.2365).astype(int), True)
+        best_mcc= eval_mcc(y_test, predictions)
         print 'tree limit:', limit
         print 'mcc:', best_mcc
 
@@ -310,10 +315,63 @@ def train_model():
 
     precision, recall, threshold = precision_recall_curve(y_test, y_pred)
     avg_pres_score = average_precision_score(y_test, y_pred)
-    plot_precision_recall([(precision, recall, avg_pres_score, 'pr')])
+    plot_precision_recall([(precision, recall, avg_pres_score, 'pr')],'pr_xgb.png')
+    print matthews_corrcoef(y_test, y_pred)
+
+
+def train_rnf_model(features, X_train, X_test, y_train, y_test):
+    params = {
+        'max_depth': 7
+        , 'min_weight_fraction_leaf': 0
+        , 'n_jobs': 3
+        , 'n_estimators': 400
+        , 'max_features': None
+    }
+    print 'Fitting Random Forest'
+    train_predictions = None
+
+    X_train = X_train.fillna(999999)
+    X_test = X_test.fillna(999999)
+
+    folds = 5
+    for i in range(folds):
+        print 'Fold:', i
+        params['random_state'] = i
+
+        mcc_eval_scorer = make_scorer(mcc_eval, greater_is_better=True)
+
+        clf = RandomForestClassifier(**params)
+
+        clf.fit(X_train, y_train)
+
+        print 'score: ', mcc_eval_scorer(clf, X_train, y_train)
+
+        for l, s in enumerate(cross_val_score(clf, X_train, y_train, scoring=mcc_eval_scorer, cv=5, n_jobs=3)):
+            print '[{0}] train-MCC:{1}'.format(l, s)
+
+        predictions = clf.predict(X_test)
+
+        if train_predictions is None:
+            train_predictions = predictions
+        else:
+            train_predictions += predictions
+
+    best_proba, best_mcc, y_pred = eval_mcc(y_test, train_predictions / folds, True)
+    print 'best_proba, best_mcc : ', best_proba, best_mcc
+    y_pred = (train_predictions / folds > 0.2365).astype(int)
+    precision, recall, threshold = precision_recall_curve(y_test, y_pred)
+    avg_pres_score = average_precision_score(y_test, y_pred)
+    plot_precision_recall([(precision, recall, avg_pres_score, 'pr')], 'pr_rnf.png')
     print matthews_corrcoef(y_test, y_pred)
 
 
 print 'started - ' + str(datetime.datetime.now())
-train_model()
+
+features, X_train, X_test, y_train, y_test = get_train_test_data()
+
+train_xgb_model(features, X_train, X_test, y_train, y_test)
+gc.collect()
+#train_rnf_model(features, X_train, X_test, y_train, y_test)
+#gc.collect()
+
 print 'finished - ' + str(datetime.datetime.now())
